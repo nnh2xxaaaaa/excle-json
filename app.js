f7775f985422d8720090af0a9cc3042f0d941bb0
nnh2xxaaaaa <ngonhathuy6878@gmail.com>
2023-06-01 14:29:17 +0700

    done

--- app.js
+++ app.js
@@ -1,60 +1,96 @@
-import Input from "./im.js";
-const fs = require("fs");
-const ExcelJS = require("exceljs");
+import inputData from "./newInputData.json" assert { type: "json" };
+import newOutputData from "./newInputData.json" assert { type: "json" };
+import fs from "fs/promises";
+import ExcelJS from "exceljs";
+
 const wb = new ExcelJS.Workbook();
+// add file excel
+
 const fileName = "input.xlsx";
 
 wb.xlsx.readFile(fileName).then(() => {
-  console.log(Input);
+  //select sheet  file in excel
   const ws = wb.getWorksheet(5);
-  ws.getRows().map((x) => console.log(x));
+  //select Coloumn
+  const filter_shipto_party_number = ws.getColumn(5).values;
+  //select Coloumn
+  const filter_Trucking_Number = ws.getColumn(6).values;
 
-  const c1 = ws.getRow(2);
-  const c2 = ws.getColumn(5);
-  const filter_tracking_same = new Set(c2.values);
-  filter_tracking_same.forEach((e, i) => {
-    if (e === undefined || e === "Shipto party number") {
-      filter_tracking_same.delete(e);
+  //sum up the points in 1 car
+  const test = [];
+  for (let i = 1; i < filter_shipto_party_number.length; i++) {
+    if (i >= 2 && filter_Trucking_Number[i] != filter_Trucking_Number[i - 1]) {
+      test.push({
+        trucking_number: filter_Trucking_Number[i],
+        shipto_party_number: [{ location_code: filter_shipto_party_number[i] }],
+      });
+    } else if (
+      i >= 2 &&
+      filter_shipto_party_number[i] != filter_shipto_party_number[i - 1] &&
+      filter_Trucking_Number[i] == filter_Trucking_Number[i - 1]
+    ) {
+      test[test.length - 1].shipto_party_number.push({
+        location_code: filter_shipto_party_number[i],
+      });
     }
-  });
+  }
 
+  // creat data json
   const dict = {
     solutions: [
       {
-        route: [],
+        routes: [],
       },
     ],
   };
-
-  filter_tracking_same.forEach((e) => {
-    dict.solutions[0].route.push({
-      element: [
-        {
-          location_code: e,
-        },
-      ],
-    });
-  });
-
-  const jsonData = JSON.stringify(dict);
-
-  fs.writeFile("data.json", jsonData, "utf8", (err) => {
-    if (err) {
-      console.error("An error occurred:", err);
-    } else {
-      console.log("JSON file has been created.");
+  // find lat lng
+  const findLatLng = (location_code) => {
+    return inputData.locations.find((x) => x.locationCode == location_code);
+  };
+  // add depotcenter
+  const findDepot = newOutputData["locations"];
+  const dataDepot = "DEPOT";
+  const def = [];
+  findDepot.map((x, i) => {
+    if (x.lTypes.includes(dataDepot)) {
+      def.push({
+        location_code: x.locationCode,
+        lat: x.lat,
+        lng: x.lng,
+      });
     }
   });
-});
 
-// const fs = require("fs");
-// const ExcelJS = require("exceljs");
-// const wb = new ExcelJS.Workbook();
-// const fileName = "input.xlsx";
-// wb.xlsx.readFile(fileName).then(() => {
-//   const ws = wb.getWorksheet(5);
-//   const all_row = ws.getRows(2, 10);
-//   all_row.forEach((x, i) => {
-//     console.log(x.values, i);
-//   });
-// });
+  // add data from data json
+  test.forEach((x, i) => {
+    const currentEl = [...def];
+    x.shipto_party_number.map((xx) => {
+      const location = findLatLng(xx.location_code);
+      xx.lat = location?.lat;
+      xx.lng = location?.lng;
+      currentEl.push(xx);
+    });
+    dict.solutions[0].routes.push({
+      elements: currentEl,
+    });
+  });
+  //delete  point overlap
+  for (const solution of dict.solutions) {
+    for (const route of solution.routes) {
+      route.elements = Array.from(
+        new Set(route.elements.map(JSON.stringify))
+      ).map(JSON.parse);
+    }
+  }
+
+  // export default file data.json
+  const jsonData = JSON.stringify(dict);
+
+  fs.writeFile("data.json", jsonData, "utf8")
+    .then(() => {
+      console.log("JSON file has been created.");
+    })
+    .catch((err) => {
+      console.error("An error occurred:", err);
+    });
+});
