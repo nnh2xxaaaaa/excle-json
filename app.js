eb5205f676b84acadcfb971e6715cb4218b44ca8
nnh2xxaaaaa <ngonhathuy6878@gmail.com>
2023-06-12 13:42:43 +0700

    filter_ok code

--- app.js
+++ app.js
@@ -6,17 +6,33 @@ import ExcelJS from "exceljs";
 const wb = new ExcelJS.Workbook();
 // add file excel
 
-const fileName = "input.xlsx";
+const fileName = "Actual_test_206.xlsx";
 
 wb.xlsx.readFile(fileName).then(() => {
   //select sheet  file in excel
-  const ws = wb.getWorksheet(5);
+  const ws = wb.getWorksheet(3);
   //select Coloumn
   const filter_shipto_party_number = ws.getColumn(5).values;
   //select Coloumn
-  const filter_Trucking_Number = ws.getColumn(6).values;
+  const filter_Trucking_Number = ws.getColumn(7).values;
 
   //sum up the points in 1 car
+
+  const groups = {};
+  for (let i = 2; i < filter_Trucking_Number.length; i++) {
+    const tt = filter_Trucking_Number[i];
+    const x = filter_shipto_party_number[i];
+
+    if (groups.hasOwnProperty(tt)) {
+      if (!groups[tt].includes(x)) {
+        groups[tt].push(x);
+      }
+    } else {
+      groups[tt] = [x];
+    }
+  }
+  console.log(groups);
+
   const test = [];
   for (let i = 1; i < filter_shipto_party_number.length; i++) {
     if (i >= 2 && filter_Trucking_Number[i] != filter_Trucking_Number[i - 1]) {
@@ -34,6 +50,34 @@ wb.xlsx.readFile(fileName).then(() => {
       });
     }
   }
+  console.log(test);
+
+  // Create an object to store merged arrays
+  const mergedData = {};
+
+  // Iterate over each object in the data array
+  test.forEach((obj) => {
+    const { trucking_number, shipto_party_number } = obj;
+
+    // If the trucking_number already exists in the mergedData object, append the shipto_party_number
+    if (mergedData[trucking_number]) {
+      mergedData[trucking_number].push(...shipto_party_number);
+    } else {
+      // Otherwise, create a new key-value pair with the trucking_number and shipto_party_number array
+      mergedData[trucking_number] = shipto_party_number;
+    }
+  });
+
+  // Convert the mergedData object back to an array
+  const mergedArray = Object.entries(mergedData).map(
+    ([trucking_number, shipto_party_number]) => ({
+      trucking_number,
+      shipto_party_number,
+    })
+  );
+
+  // Output the merged array
+  // console.log(mergedArray);
 
   // creat data json
   const dict = {
@@ -61,8 +105,8 @@ wb.xlsx.readFile(fileName).then(() => {
     }
   });
 
-  // add data from data json
-  test.forEach((x, i) => {
+  // // add data from data json
+  mergedArray.forEach((x, i) => {
     const currentEl = [...def];
     x.shipto_party_number.map((xx) => {
       const location = findLatLng(xx.location_code);
@@ -71,6 +115,7 @@ wb.xlsx.readFile(fileName).then(() => {
       currentEl.push(xx);
     });
     dict.solutions[0].routes.push({
+      vehicle_code: x.trucking_number,
       elements: currentEl,
     });
   });
@@ -83,10 +128,9 @@ wb.xlsx.readFile(fileName).then(() => {
     }
   }
 
-  // export default file data.json
   const jsonData = JSON.stringify(dict);
 
-  fs.writeFile("data.json", jsonData, "utf8")
+  fs.writeFile("Actual_Test_206.json", jsonData, "utf8")
     .then(() => {
       console.log("JSON file has been created.");
     })
